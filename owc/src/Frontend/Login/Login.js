e1805bdc92961ab6390a3fdcf33f2a9680c3f73b
Rohit <rohitmore1848@gmail.com>
2023-03-09 22:59:10 +0530

    url

--- owc/src/Frontend/Login/Login.js
+++ owc/src/Frontend/Login/Login.js
@@ -79,7 +79,7 @@ export default function Login() {
                       sessionStorage.setItem("role",values.role);
                       sessionStorage.setItem("name",values.name);
                       sessionStorage.setItem("phoneno",values.phoneno);
-                      sessionStrorage.setItem("orders",values.orders)
+                      sessionStorage.setItem("orders",values.orders);
                       if(email===user.email){
                        setvali(true)
                       }
--- owc/src/config.js
+++ owc/src/config.js
@@ -1,5 +1,5 @@
 const url={
     server:"https://cdac-project.onrender.com" 
-   // server:"http://localhost:5000" 
+//    server:"http://localhost:5000" 
 }
 export default url
